6109c48c776b6ce520cce1c7bba6470aa5c6a4d9
spyseal4345 <spyseal4345@gmail.com>
2023-09-13 19:20:45 +0700

    the last commit

--- javascript/login-signin.js
+++ javascript/login-signin.js
@@ -6,8 +6,8 @@ let signupBtn = document.querySelector("#signup");
 let loginBtn = document.querySelector("#login");
 let pwShowHide = document.querySelector(".hide");
 // show-up
-formOpenBtn.addEventListener("click",() => loginContainer.classList.add("show"))
-formOpenBtn.addEventListener("click",() => loginContainer.classList.remove("show"))
+// formOpenBtn.addEventListener("click",() => loginContainer.classList.add("show"))
+// formOpenBtn.addEventListener("click",() => loginContainer.classList.remove("show"))
 // changing-form
 signupBtn.addEventListener("click", (e) => {
   e.preventDefault();
